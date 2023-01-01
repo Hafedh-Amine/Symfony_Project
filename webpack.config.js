b564074d7a7f71e007e2e531ca58e9d6f88c7694
Hafedh-Amine <113897973+Hafedh-Amine@users.noreply.github.com>
2023-01-01 19:51:44 +0100

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
+
     .copyFiles({
         from: './assets/images',
 
@@ -30,7 +31,6 @@ Encore
         from: './assets/css',
          to: 'css/[path][name].[ext]',
   })
-
     /*
      * ENTRY CONFIG
      *
@@ -75,7 +75,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    .enableSassLoader()
+     .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -88,7 +88,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    .autoProvidejQuery()
+     .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
